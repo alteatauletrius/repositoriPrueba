93f40cdb1922be4c46b1c8e2d2f895d255f81f6c
altea.tauletrius1 <altea.tauletrius1@ciclosformativosceu.es>
2024-11-19 18:14:52 +0100

    3. Add despedir a rama de pruebas

--- src/App.java
+++ src/App.java
@@ -2,5 +2,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Mas cosas");
+        despedir();
+    }
+
+    private static void despedir() {
+        System.out.println("Adiós");
     }
 }
