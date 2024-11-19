fc26059dc035f73c3a1ac6bea34c12ef5d3debd7
altea.tauletrius1 <altea.tauletrius1@ciclosformativosceu.es>
2024-11-19 18:27:21 +0100

    4. Delete de despedir

--- src/App.java
+++ src/App.java
@@ -2,7 +2,11 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Mas cosas");
+        despedir();
+    }
 
+    private static void despedir() {
+        System.out.println("Adios!");
     }
 
 
